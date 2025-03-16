ecd94a9358172e13b720fc33e3f58ca12447c6eb
anujuliet <anujulietmanojk@gmail.com>
2025-03-16 12:32:08 +0530

    Create ViewTransactionHistory.js

---
+++ src/pages/ViewTransactionHistory.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ViewTransactionHistory = () => {
+  return (
+    <div className="container mt-5 text-center">
+      <h2 className="fw-bold">Transaction History</h2>
+    </div>
+  );
+};
+
+export default ViewTransactionHistory;
